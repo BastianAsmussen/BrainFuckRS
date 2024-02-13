e44acfead2c7a63f9401137857efaec1751c6f9d
Bastian Asmussen <bastian@asmussen.tech>
2024-02-13 19:11:26 +0100

    Add docu-comment.

--- lang/src/util/files.rs
+++ lang/src/util/files.rs
@@ -1,5 +1,19 @@
 use std::{ffi::OsStr, path::Path};
 
+/// Reads a BrainFuck file from a given path.
+///
+/// # Arguments
+///
+/// * `path` - The path to the `.bf` file.
+///
+/// # Returns
+///
+/// * `Result<String, std::io::Error>` - The contents of the file.
+///
+/// # Errors
+///
+/// * `InvalidInput` - If the file extension isn't `.bf`.
+/// * `std::io::Error` - If any I/O error occurs while trying to read.
 pub fn read_bf_file(path: &str) -> Result<String, std::io::Error> {
     let path = Path::new(path);
     if path.extension() == None || path.extension() != Some(&OsStr::new("bf")) {
