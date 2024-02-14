7b531af486bb0d7c588ab56fb38d5180211c7e47
Bastian Asmussen <bastian@asmussen.tech>
2024-02-14 09:16:25 +0100

    Add timings.

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,29 @@ mod args;
 use crate::args::Args;
 use anyhow::Result;
 use clap::Parser as ArgsParser;
+use lang::interpreter::Interpreter;
 use lang::lexer::Lexer;
 use lang::parser::Parser;
-use lang::util;
+use lang::utils;
+use lang::utils::timer;
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    let source = util::files::read_bf_file(&args.file)?;
 
-    let tokens = Lexer::new(&source).lex()?;
-    println!("{tokens:#?}");
+    let (source, elapsed) = timer::time(|| utils::files::read_bf_file(&args.file));
+    let source = source?;
+    println!("Read file in {elapsed:?}.");
 
-    let ast = Parser::new(&tokens).parse()?;
-    println!("{ast:#?}");
+    let (tokens, elapsed) = timer::time(|| Lexer::new(&source).lex());
+    let tokens = tokens?;
+    println!("Lexed in {elapsed:?}.");
+
+    let (ast, elapsed) = timer::time(|| Parser::new(&tokens).parse());
+    let ast = ast?;
+    println!("Parsed in {elapsed:?}.");
+
+    let (_, elapsed) = timer::time(|| Interpreter::new(&ast).run());
+    println!("Interpreted in {elapsed:?}.");
 
     Ok(())
 }
